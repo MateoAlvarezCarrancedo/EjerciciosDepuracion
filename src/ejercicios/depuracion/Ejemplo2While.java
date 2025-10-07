d5ccb72f7c26e78be0b2b1b8914c42cd8778a509
mateoac26 <mateoac88@educastur.es>
2025-10-07 15:32:32 +0200

    segundo commit

--- src/ejercicios/depuracion/Ejemplo2While.java
+++ src/ejercicios/depuracion/Ejemplo2While.java
@@ -8,8 +8,10 @@ public class Ejemplo2While {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n, contador = 0;
+
         System.out.print("Introduce un número: ");
         n = sc.nextInt();
+        
         while (contador< n){
                System.out.println(" * ");
                contador++;

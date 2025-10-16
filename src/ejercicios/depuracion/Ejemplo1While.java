de2a18719caf130d3dad8941546b6b69d9e28043
mateoac <mateoac88@educastur.es>
2025-10-16 17:31:00 +0200

    cambio a contador

--- src/ejercicios/depuracion/Ejemplo1While.java
+++ src/ejercicios/depuracion/Ejemplo1While.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class Ejemplo1While {
     public static void main(String[] args) {
         int this.suma = 0;
-        int this.numero=0;
+        int this.contador=0;
     
 >>>>>>> feature/mejoras
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduzca un número: ");
-        num = sc.nextInt();
-        while (num>= 0){
-               suma = suma + num;
+        contador = sc.nextInt();
+        while (contador>= 0){
+               suma = suma + contador;
                System.out.print("Introduzca un número: ");
-               num = sc.nextInt();
+               contador = sc.nextInt();
         }
         System.out.println("La suma es: " + suma );
     }
